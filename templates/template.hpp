111d52c7e356bea0b07eb5d5cdab639e64dae049
Tyler Wayne <tylerwayne3@gmail.com>
2023-04-22 21:16:02 -0700

    update c templates, add txt template

--- templates/template.hpp
+++ templates/template.hpp
@@ -1,9 +1,9 @@
-//
+// 
 // -----------------------------------------------------------------------------
-// filename.c
+// file.hpp
 // -----------------------------------------------------------------------------
 //
-// Copyright (c) 2023 Tyler Wayne
+// Copyright (c) 2022 Tyler Wayne
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
